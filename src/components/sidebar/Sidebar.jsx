dc3a4622a156ac203a92140eeb6df64eb85c2110
romeoscript <romeobourne211@gmail.com>
2022-09-10 04:27:20 +0100

    homepage responsiveness

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import logo from "../../imgs/logo.png";
-import { UilEstate } from "@iconscout/react-unicons";
+import { UilSignOutAlt, UilBars } from "@iconscout/react-unicons";
 import Sidebarmenu from "./Sidebarmenu";
+import { motion } from "framer-motion";
 // import { UilEstate } from "@iconsout/react-unicons";
-import {SideBarData} from '../../data/data'
-const Sidebar = () => {
-  return (
-    <div className="Sidebar">
-      {/* logo */}
-      <div className="logo">
-        <img src={logo} alt="" />
-        <span>
-          {" "}
-          Nano<span>co</span>des
-        </span>
-      </div>
+import { SideBarData } from "../../data/data";
 
-      {/* menu */}
-      <div className="menu">
-       
-        {SideBarData.map((item,index)=> <Sidebarmenu key={index} item={item} />)}
+const Sidebar = () => {
+  const [expanded, setExpanded] = useState(true);
+const sidebarVariants = {
+  true:{
+    left : '0'
+  },
+  false:{
+    left: '-60%'
+  }
+}
+  return (
+    <>
+      <div className="bars" style={expanded?{left:'60%'}:{left:'0%'}}
+      onClick={()=> setExpanded(!expanded)}
+      >
+        <UilBars />
       </div>
-    </div>
+      <motion.div className="Sidebar"
+      variants={sidebarVariants}
+      animate={window.innerWidth<=768? `${expanded}` : ''}
+      >
+        {/* logo */}
+        <div className="logo">
+          <img src={logo} alt="" />
+          <span>
+            {" "}
+            Nano<span>co</span>des
+          </span>
+        </div>
+
+        {/* menu */}
+        <div className="menu">
+          {SideBarData.map((item, index) => (
+            <Sidebarmenu key={index} item={item} />
+          ))}
+        </div>
+      </motion.div>
+    </>
   );
 };
 
